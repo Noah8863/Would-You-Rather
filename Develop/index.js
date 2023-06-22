4ae22531cd2c669d30b99989a598e591547c4e6f
Noah Hoffman <stacknoah99@gmail.com>
2023-06-22 08:19:22 -0600

    treeshake for final commit

--- Develop/index.js
+++ Develop/index.js
@@ -16,93 +16,71 @@ const rightResults = document.getElementById("rightResults");
 //Screen Variables
 const gameContainer = document.getElementById("questionsContainer");
 
-
-//Start Game Event Listener 
-startBtn.addEventListener("click", StartGame)
-startBtn.addEventListener("click", showQuestion)
-
+//Start Game Event Listener
+startBtn.addEventListener("click", StartGame);
+startBtn.addEventListener("click", showQuestion);
 
 //In Game Event Listener
 leftChoice.addEventListener("click", showQuestion);
 rightChoice.addEventListener("click", showQuestion);
 
-
-function StartGame(){
-    leftSlider.classList.add("slide-left")
-    rightSlider.classList.add("slide-right")
-    startBtn.classList.add("hide")
-    startingInstructions.classList.add("hide")
-    btnContainer.classList.add("hide")
-    setTimeout(function FadeIn(){
-        gameContainer.classList.add("fadeIn")
-    }, 1);
+function StartGame() {
+  leftSlider.classList.add("slide-left");
+  rightSlider.classList.add("slide-right");
+  startBtn.classList.add("hide");
+  startingInstructions.classList.add("hide");
+  btnContainer.classList.add("hide");
+  setTimeout(function FadeIn() {
+    gameContainer.classList.add("fadeIn");
+  }, 1);
 }
 
-
-const url = 'https://would-you-rather.p.rapidapi.com/wyr/random';
+const url = "https://would-you-rather.p.rapidapi.com/wyr/random";
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'ca75b62c85mshdb48cab1b700d04p1387f4jsn794cb6953840',
-		'X-RapidAPI-Host': 'would-you-rather.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ca75b62c85mshdb48cab1b700d04p1387f4jsn794cb6953840",
+    "X-RapidAPI-Host": "would-you-rather.p.rapidapi.com",
+  },
 };
 
 async function showQuestion() {
+  const url = "https://would-you-rather.p.rapidapi.com/wyr/random";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "ca75b62c85mshdb48cab1b700d04p1387f4jsn794cb6953840",
+      "X-RapidAPI-Host": "would-you-rather.p.rapidapi.com",
+    },
+  };
 
-    const url = 'https://would-you-rather.p.rapidapi.com/wyr/random';
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'ca75b62c85mshdb48cab1b700d04p1387f4jsn794cb6953840',
-            'X-RapidAPI-Host': 'would-you-rather.p.rapidapi.com'
-        }
-    };
-    
-    try {
+  try {
+    //fetch the response from API server
+    const response = await fetch(url, options);
+    const result = await response.json();
 
-        //fetch the response from API server
-        const response = await fetch(url, options);
-        const result = await response.json();
+    //Isolate the question from the response object
+    var question = result[0].question;
 
-        //Isolate the question from the response object
-        var question = result[0].question;
+    //Split the response into each individual word
+    var brokenQuestion = question.split(" ");
 
-        //Split the response into each individual word
-        var brokenQuestion = question.split(' ');
+    //Find the Index of the "or" keyword
+    var orIndex = brokenQuestion.indexOf("or");
 
-        //Find the Index of the "or" keyword
-        var orIndex = brokenQuestion.indexOf("or")
+    //Create the first option before the "or" word
+    var firstHalf = brokenQuestion.slice(0, orIndex);
+    var firstChoice = firstHalf.join(" ");
 
-        //Create the first option before the "or" word
-        var firstHalf = brokenQuestion.slice(0,orIndex)
-        var firstChoice = firstHalf.join(" ")
-        
-        //Create the second option after the "or" word and remove the word "or"
-        var secondHalf = brokenQuestion.slice(orIndex)
-        secondHalf.shift()
-        var secondChoice = secondHalf.join(" ")
+    //Create the second option after the "or" word and remove the word "or"
+    var secondHalf = brokenQuestion.slice(orIndex);
+    secondHalf.shift();
+    var secondChoice = secondHalf.join(" ");
 
-        //Inject the options into the buttons
-        leftChoice.innerHTML = firstChoice;
-        rightChoice.innerHTML = secondChoice;
-
-    } catch (error) {
-        console.error(error);
-    }
+    //Inject the options into the buttons
+    leftChoice.innerHTML = firstChoice;
+    rightChoice.innerHTML = secondChoice;
+  } catch (error) {
+    console.error(error);
+  }
 }
-
-
-  
-        
-
-
-
-
-
-
-
-
-
-
-
